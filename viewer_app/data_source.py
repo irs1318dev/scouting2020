9dfb89c8870ad9205e4640ce57348fbb14c2b314
Stacy Irwin <stacy.irwin@ischool.berkeley.edu>
2020-02-17 15:26:21 -0800

    Refactored data source file.

--- viewer_app/data_source.py
+++ viewer_app/data_source.py
@@ -1,31 +1,84 @@
 """Gets data for viewer classes.
 Meghana Annamaneni 2/17/2020
+Stacy Irwin 2/17/2020
+
+Contents
+--------
+    DataSource: A class that can extract scouting data from either the
+        scouting system's SQL database, or from a Python pickle file.
 """
+import pickle
+
 import pandas as pd
 
 import server.model.connection as smc
 import server.model.event as sme
 
+
 class DataSource:
     def __init__(self, fname=None, event=None, season=None):
-        if event is not None:
-            sme.EventDal.set_current_event(event, season)
+        self.measures = None
+        self.schedule = None
+        self.teams = None
+        self.event = event
+        self.season = season
+        self.fname = fname
+        self.from_sql = False
+
+        if self.fname is not None:
+            self._load_from_file()
+        else:
+            self.from_sql = True
+            self._load_from_sql()
+
+    def _load_from_file(self):
+        with open(self.fname, 'rb') as data_file:
+            data = pickle.load(data_file)
+        self.measures = data['measures']
+        self.schedule = data['schedule']
+        self.teams = data['teams']
+        self.event = data['event']
+        self.season = data['season']
+
+    def _load_from_sql(self):
+        if self.event is not None and self.season is not None:
+            sme.EventDal.set_current_event(self.event, self.season)
+
+        # Load measures data
         conn = smc.pool.getconn()
-        sql = "SELECT * FROM vw_measures"
+        sql = "SELECT * FROM vw_measures;"
         self.measures = pd.read_sql(sql, conn)
 
         evt = sme.EventDal.get_current_event()
         evt_id = evt[0]
+        self.event = evt[1]
+        self.season = evt[2]
 
         sql = """
         SELECT * FROM schedules
-        WHERE event_id=%s
-        ORDER BY match, alliance;
-        """
+            WHERE event_id=%s
+            ORDER BY match, alliance;"""
         self.schedule = pd.read_sql(sql, conn, params=[str(evt_id)])
 
         sql = """
         SELECT * FROM teams
-        WHERE teams.name IN (SELECT team FROM schedules WHERE event_id = %s);
-        """
+            WHERE teams.name IN
+                (SELECT team FROM schedules WHERE event_id = %s);"""
         self.teams = pd.read_sql(sql, conn, params=[str(evt_id)])
+
+    def refresh(self, fname=None):
+        if fname is not None:
+            self.fname = fname
+        if self.from_sql:
+            self._load_from_sql()
+        else:
+            self._load_from_file()
+
+    def write_file(self, fname = None):
+        data = {'measures': self.measures, 'schedule': self.schedule,
+                'teams': self.teams, 'event': self.event,
+                'season': self.season}
+
+        with open(fname, 'wb') as data_file:
+            pickle.dump(data, data_file)
+
---
+++ viewer_app/tests/test_data_source.py
@@ -0,0 +1,42 @@
+import os
+import os.path
+
+import pandas as pd
+
+import viewer_app.data_source as app_data
+
+test_event_name = 'test_event_2'
+test_season = '2020'
+test_file_name = r'\data_write_test.pickle'
+
+
+def test_data_source():
+    data = app_data.DataSource(event=test_event_name, season=test_season)
+    assert isinstance(data, app_data.DataSource)
+    assert isinstance(data.measures, pd.DataFrame)
+    assert data.measures.shape[0] > 1000
+    assert data.measures.shape[1] == 20
+
+    assert isinstance(data.schedule, pd.DataFrame)
+    assert isinstance(data.teams, pd.DataFrame)
+    assert data.teams.shape[1] == 7
+    assert data.teams.shape[0] > 20
+
+    assert data.event == test_event_name
+    assert data.season == test_season
+
+
+def test_write_file():
+    path = os.path.abspath(__file__)
+    for _ in range(4):
+        path = os.path.dirname(path)
+    data = app_data.DataSource(event=test_event_name, season=test_season)
+    fpath = path + test_file_name
+    data.write_file(fpath)
+    file_data = app_data.DataSource(fpath)
+    os.remove(fpath)
+    assert isinstance(file_data, app_data.DataSource)
+    assert file_data.event == test_event_name
+    assert file_data.season == test_season
+    assert not file_data.from_sql
+
