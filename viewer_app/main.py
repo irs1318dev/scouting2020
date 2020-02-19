f3b47497edafaa6483afd531bd05f661b702ba7c
Stacy Irwin <stacy.irwin@ischool.berkeley.edu>
2020-02-18 17:36:39 -0800

    Added comments to viewer_app/data_source

--- viewer_app/main.py
+++ viewer_app/main.py
@@ -1,7 +1,7 @@
 import bokeh.plotting as plt
 
-import viewer_app.viewer as svv
-
-viewer = svv.Viewer()
-layout = viewer.layout_6t('001-q')
-plt.curdoc().add_root(layout)
+# import viewer_app.viewer as svv
+#
+# viewer = svv.Viewer()
+# layout = viewer.layout_6t('001-q')
+# plt.curdoc().add_root(layout)
