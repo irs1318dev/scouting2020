695d1b34738c5fbd136921743ae43374378a9f3e
emily <emilyrobinsonswim@hotmail.com>
2020-02-24 20:08:10 -0800

    create file handler

--- viewer_app/main.py
+++ viewer_app/main.py
@@ -6,6 +6,7 @@ import bokeh.models as bk_models
 import viewer_app.data_source as va_data_source
 import viewer_app.sixteam as va_sixteam
 import viewer_app.oneteam as va_oneteam
+import viewer_app.file_management as va_fm
 
 if sys.argv[1] == 'sql':
     data_source = va_data_source.DataSource(event='test_event_2',
@@ -21,5 +22,12 @@ panels.append(sixteam.panel('001-q'))
 oneteam_tasks = ['launchOuter', 'climbPosition']
 oneteam = va_oneteam.OneTeam(data_source)
 panels.append(oneteam.panel_1t('1318', ['launchOuter', 'launchLower']))
+
+file_manager = va_fm.DataFile(data_source)
+panels.append(file_manager.panel_file_management())
+
 tabs = bk_models.Tabs(tabs=panels)
 bk_plt.curdoc().add_root(tabs)
+
+
+
