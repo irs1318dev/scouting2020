04218bbcd995213e9ac26a32f72e4c2d209744b8
emily <emilyrobinsonswim@hotmail.com>
2020-02-24 20:08:10 -0800

    create file handler

--- viewer_app/main.py
+++ viewer_app/main.py
@@ -4,6 +4,7 @@ import bokeh.models as bk_models
 import viewer_app.data_source as va_data_source
 import viewer_app.sixteam as va_sixteam
 import viewer_app.oneteam as va_oneteam
+import viewer_app.buttons as va_buttons
 
 data_source = va_data_source.DataSource(event='test_event_2', season='2020')
 
@@ -11,9 +12,14 @@ panels = []
 
 sixteam = va_sixteam.SixTeam(data_source)
 panels.append(sixteam.panel('001-q'))
-
 oneteam_tasks = ['shootUpper', 'climbPosition']
 oneteam = va_oneteam.OneTeam(data_source)
 panels.append(oneteam.panel_1t('1318', ['shootUpper', 'shootLower']))
+
+plot_objects = [sixteam, oneteam]
+
 tabs = bk_models.Tabs(tabs=panels)
 bk_plt.curdoc().add_root(tabs)
+
+
+
