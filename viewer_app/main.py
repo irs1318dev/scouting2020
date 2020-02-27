adf101bc0dfcba8ec9bf53a18453357509e48ea6
emily <emilyrobinsonswim@hotmail.com>
2020-02-26 20:39:17 -0800

    simplified update graphs button code

--- viewer_app/main.py
+++ viewer_app/main.py
@@ -7,6 +7,8 @@ import viewer_app.oneteam as va_oneteam
 import viewer_app.file_management as va_fm
 
 data_source = va_data_source.DataSource(event='test_event_2', season='2020')
+# data_source.write_file(fname='test_pickle')
+# data_source = va_data_source.DataSource(fname='test_pickle')
 
 panels = []
 
@@ -18,8 +20,9 @@ oneteam = va_oneteam.OneTeam(data_source)
 panels.append(oneteam.panel_1t('1318', ['launchOuter', 'launchLower']))
 
 plot_object_data = [sixteam.data, oneteam.data]
-file_manager = va_fm.DataFile()
-panels.append(file_manager.panel_file_management(plot_object_data))
+file_manager = va_fm.DataFile(data_source)
+panels.append(file_manager.panel_file_management())
+    # plot_object_data))
 
 tabs = bk_models.Tabs(tabs=panels)
 bk_plt.curdoc().add_root(tabs)
